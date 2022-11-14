851b30fe8647d28123f649d3b4b92cc0b9ef0613
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-11-14 21:33:55 +0100

    create

--- lib/structs.go
+++ lib/structs.go
@@ -1,13 +1,13 @@
 package users
 import "reflect"
 type User struct {
-	ID                 	string `json:"id"`
-	UserName 			string `json:"userName"`
-	LastName            string `json:"lastName"`
-	FirstName        	string `json:"firstName"`
+	id                 	string `json:"id"`
+	userName 			string `json:"userName"`
+	lastName            string `json:"lastName"`
+	firstName        	string `json:"firstName"`
 	gender             	string `json:"gender"`
-	Mail             	string `json:"mail"`
-	Phone             	string `json:"phone"`
+	mail             	string `json:"mail"`
+	phone             	string `json:"phone"`
 	address				struct {
 		Street             	string `json:"street"`
 		HouseNr             string `json:"houseNr"`
@@ -18,12 +18,12 @@ type User struct {
 }
 
 type Skill struct {
-	ID              	string      `json:"id"`
-	Name             	string      `json:"name"`
+	id              	string      `json:"id"`
+	name             	string      `json:"name"`
 	level               string      `json:"level"`
 }
 // To get embedded JSON fields
-func (v Event) GetField(field string, value string) string {
+func (v User) GetField(field string, value string) string {
 	r := reflect.ValueOf(v)
 	f := reflect.Indirect(r).FieldByName(field)
 	return f.FieldByName(value).String()
--- main.go
+++ main.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+        "github.com/neo4j/neo4j-go-driver/neo4j"
+)
+func main() {
+	// connect to database
+	session, driver, err := ConnectToDB()
+	if err != nil {
+		log.Fatalln("Error connecting to Database")
+		log.Fatalln(err)
+	}
+	log.Println("Connected to Neo4j")
+	// Close driver and session after func ends
+	defer driver.Close()
+	defer session.Close()
+	// pass the session to the model layer
+	events.SetDB(session)
+	// populate templates
+	controller.Startup()
+	// listen on specified port
+	log.Println("Starting to listen..")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func ConnectToDB() (neo4j.Session, neo4j.Driver, error) {
+	// define driver, session and result vars
+	var (
+		driver  neo4j.Driver
+		session neo4j.Session
+		err     error
+	)
+	// initialize driver to connect to localhost with ID and password
+	if driver, err = neo4j.NewDriver("bolt://localhost:7687", neo4j.BasicAuth("angad", "angad", "")); err != nil {
+		return nil, nil, err
+	}
+	// Open a new session with write access
+	if session, err = driver.Session(neo4j.AccessModeWrite); err != nil {
+		return nil, nil, err
+	}
+	return session, driver, nil
+}
