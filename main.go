af2dc36f4a4103596f4ed2f93a8006f7965cbbc9
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-11-19 22:07:51 +0100

    plan

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"database/sql"
-    "github.com/gin-gonic/gin"
-	"github.com/DrBackmischung/Nachhilfe-UserService/rest"
-	_ "github.com/go-sql-driver/mysql"
 	"log"
+
+	"github.com/DrBackmischung/Nachhilfe-UserService/rest"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	
+
 	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/userservice")
 	if err != nil {
-        log.Print(err.Error())
-    }
+		log.Print(err.Error())
+	}
 	defer db.Close()
 
 	router := gin.Default()
@@ -27,7 +28,7 @@ func main() {
 
 	router.GET("/skills/:id/users", rest.GetUsersForSkill(db))
 	router.GET("/users/:id/skills", rest.GetSkillsForUser(db))
-	
+
 	//router.GET("/login", rest.Login(db))
 
 	// CREATE
@@ -45,9 +46,11 @@ func main() {
 	// DELETE
 	router.DELETE("/skills/:id", rest.DeleteSkill(db))
 	router.DELETE("/users/:id", rest.DeleteUser(db))
-	
+
 	router.DELETE("/users/:id/:skillId", rest.RemoveSkillFromUser(db))
 
+	//router.POST("/unregister", rest.Unregister(db))
+
 	router.Run("localhost:6001")
-	
+
 }
